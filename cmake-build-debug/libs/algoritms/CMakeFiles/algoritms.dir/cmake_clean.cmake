313debe5a51337ee86310df596df75db81c85610
Влад <vvipvsv@gmail.com>
2022-02-09 21:15:22 +0300

    first commit

---
+++ cmake-build-debug/libs/algoritms/CMakeFiles/algoritms.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/algoritms.dir/algoritm.c.obj"
+  "CMakeFiles/algoritms.dir/array/array.c.obj"
+  "libalgoritms.a"
+  "libalgoritms.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/algoritms.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/libs/data_structure/CMakeFiles/data_structure.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/data_structure.dir/vector/vector.c.obj"
+  "libdata_structure.a"
+  "libdata_structure.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/data_structure.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
